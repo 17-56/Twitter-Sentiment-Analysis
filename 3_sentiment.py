12a58e8a4476ac0997f09a8fc17ce2431f55536d
Olivia Bordeu Gazmuri <oli.bordeu@gmail.com>
2017-06-26 15:19:21 -0700

    Fixed bugs

--- 3_sentiment.py
+++ 3_sentiment.py
@@ -8,61 +8,72 @@ from googletrans import Translator
 from textblob import TextBlob
 import csv
 
-# Read the whole text.
-text = open('tweetsv2.txt').read()
-text=text.split('\n')
-tweets=[aux.split("|") for aux in text ]
-tweets=[aux for aux in tweets if len(aux)==4 ]
-print len(tweets)
-print tweets[0:4]
-dates={}
-dates['Pinera']=[x[1].replace("+0000 ","") for x in tweets if '@sebastianpinera' in x[2]]
-dates['Guillier']=[x[1].replace("+0000 ","") for x in tweets if '@guillier' in x[2]]
-dates['Goic']=[x[1].replace("+0000 ","") for x in tweets if '@carolinagoic' in x[2]]
-dates['Sanchez']=[x[1].replace("+0000 ","") for x in tweets if '@BeaSanchezYTu' in x[2] or '@labeasanchez' in x[2]]
-dates['Ossandon']=[x[1].replace("+0000 ","") for x in tweets if '@mjossandon' in x[2]]
-dates['Mayol']=[x[1].replace("+0000 ","") for x in tweets if '@AlbertoMayol' in x[2] or '@MayolPresidente' in x[2]]
-dates['Kast']=[x[1].replace("+0000 ","") for x in tweets if '@felipekast' in x[2] ]
+def main():
+	# Read the whole text.
+	text = open('tweetsv2.txt').read()
+	text=text.split('\n')
+	tweets=[aux.split("|") for aux in text ]
+	tweets=[aux for aux in tweets if len(aux)==4 ]
+	print len(tweets)
+	print tweets[0:4]
+	dates={}
+	dates['Pinera']=[x[1].replace("+0000 ","") for x in tweets if '@sebastianpinera' in x[2]]
+	dates['Guillier']=[x[1].replace("+0000 ","") for x in tweets if '@guillier' in x[2]]
+	dates['Goic']=[x[1].replace("+0000 ","") for x in tweets if '@carolinagoic' in x[2]]
+	dates['Sanchez']=[x[1].replace("+0000 ","") for x in tweets if '@BeaSanchezYTu' in x[2] or '@labeasanchez' in x[2]]
+	dates['Ossandon']=[x[1].replace("+0000 ","") for x in tweets if '@mjossandon' in x[2]]
+	dates['Mayol']=[x[1].replace("+0000 ","") for x in tweets if '@AlbertoMayol' in x[2] or '@MayolPresidente' in x[2]]
+	dates['Kast']=[x[1].replace("+0000 ","") for x in tweets if '@felipekast' in x[2] ]
 
-tweet={}
-tweet['Pinera']=[unicodedata.normalize('NFKD', unicode(x[2],'utf-8')).encode('ASCII', 'ignore') for x in tweets if '@sebastianpinera' in x[2]]
-tweet['Guillier']=[unicodedata.normalize('NFKD', unicode(x[2],'utf-8')).encode('ASCII', 'ignore') for x in tweets if '@guillier' in x[2]]
-tweet['Goic']=[unicodedata.normalize('NFKD', unicode(x[2],'utf-8')).encode('ASCII', 'ignore') for x in tweets if '@carolinagoic' in x[2]]
-tweet['Sanchez']=[unicodedata.normalize('NFKD', unicode(x[2],'utf-8')).encode('ASCII', 'ignore') for x in tweets if '@BeaSanchezYTu' in x[2] or '@labeasanchez' in x[2]]
-tweet['Ossandon']=[unicodedata.normalize('NFKD', unicode(x[2],'utf-8')).encode('ASCII', 'ignore') for x in tweets if '@mjossandon' in x[2]]
-tweet['Mayol']=[unicodedata.normalize('NFKD', unicode(x[2],'utf-8')).encode('ASCII', 'ignore') for x in tweets if '@AlbertoMayol' in x[2] or '@MayolPresidente' in x[2]]
-tweet['Kast']=[unicodedata.normalize('NFKD', unicode(x[2],'utf-8')).encode('ASCII', 'ignore') for x in tweets if '@felipekast' in x[2] ]
+	tweet={}
+	tweet['Pinera']=[unicodedata.normalize('NFKD', unicode(x[2],'utf-8')).encode('ASCII', 'ignore') for x in tweets if '@sebastianpinera' in x[2]]
+	tweet['Guillier']=[unicodedata.normalize('NFKD', unicode(x[2],'utf-8')).encode('ASCII', 'ignore') for x in tweets if '@guillier' in x[2]]
+	tweet['Goic']=[unicodedata.normalize('NFKD', unicode(x[2],'utf-8')).encode('ASCII', 'ignore') for x in tweets if '@carolinagoic' in x[2]]
+	tweet['Sanchez']=[unicodedata.normalize('NFKD', unicode(x[2],'utf-8')).encode('ASCII', 'ignore') for x in tweets if '@BeaSanchezYTu' in x[2] or '@labeasanchez' in x[2]]
+	tweet['Ossandon']=[unicodedata.normalize('NFKD', unicode(x[2],'utf-8')).encode('ASCII', 'ignore') for x in tweets if '@mjossandon' in x[2]]
+	tweet['Mayol']=[unicodedata.normalize('NFKD', unicode(x[2],'utf-8')).encode('ASCII', 'ignore') for x in tweets if '@AlbertoMayol' in x[2] or '@MayolPresidente' in x[2]]
+	tweet['Kast']=[unicodedata.normalize('NFKD', unicode(x[2],'utf-8')).encode('ASCII', 'ignore') for x in tweets if '@felipekast' in x[2] ]
 
+	#dates=[datetime.datetime.strptime(x,'%a %b %d %H:%M:%S %Y') for x in dates]
+	candidatos=['Pinera','Guillier','Goic','Sanchez','Ossandon','Mayol','Kast']
+	# Sacar # y @usuarios y traducir y analizar
+	sentiment={}
+	english={}
+	for c in candidatos:
+		tweet[c]=[re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",aux).replace(' RT ',' ') for aux in tweet[c]]
+		english[c]=list(map(translate,tweet[c]))
+		sentiment[c]=list(map(sentimentAnalysis,english[c]))
 
-
-#dates=[datetime.datetime.strptime(x,'%a %b %d %H:%M:%S %Y') for x in dates]
-candidatos=['Pinera','Guillier','Goic','Sanchez','Ossandon','Mayol','Kast']
-# Sacar # y @usuarios y traducir y analizar
-translator=Translator()
-sentiment={}
-english={}
-for c in candidatos:
-	tweet[c]=[re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",aux).replace(' RT ',' ') for aux in tweet[c]]
-	english[c]=[translator.translate(aux).text for aux in tweet[c]]
-	sentiment[c]=list(map(sentimentAnalysis,english[c]))
-
-with open('Analysis.csv', 'wb') as myfile:
-		wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-		wr.writerow(['date','spanish','english','sentiment','candidate'])
-		for c in candidatos:
-			for x in range(0,len(tweet[c])):
-				row=[]
-				row.append(dates[c][x])
-				row.append(tweet[c][x])
-				row.append(english[c][x])
-				row.append(sentiment[c][x])
-				row.append(c)
-				wr.writerow(row)
+	with open('Analysis.csv', 'wb') as myfile:
+			wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+			wr.writerow(['date','spanish','english','sentiment','candidate'])
+			for c in candidatos:
+				for x in range(0,len(tweet[c])):
+					row=[]
+					row.append(dates[c][x].encode('latin-1'))
+					row.append(tweet[c][x].encode('latin-1'))
+					row.append(english[c][x].encode('latin-1'))
+					row.append(sentiment[c][x])
+					row.append(c.encode('latin-1'))
+					wr.writerow(row)
 
 
 
 def sentimentAnalysis(iterator):
-	analysis = TextBlob(iterator)
-	score=analysis.sentiment.polarity
+	if iterator!="":
+		analysis = TextBlob(iterator)
+		score=analysis.sentiment.polarity
+	else:
+		score=""
 	return score
 
+def translate(iterator):
+	translator=Translator()
+	try:
+		eng=translator.translate(iterator).text
+	except:
+		eng=""
+	return eng
+
+if __name__ == '__main__':
+    main()
